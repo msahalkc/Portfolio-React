1e36182ef22f2dd0c40d827f5c0471684c212995
Muhammed Sahal K C <msahalkc@gmail.com>
2024-03-10 20:05:06 +0530

    fix: removed unnecessary code from navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,33 +2,6 @@ import React, { useEffect } from 'react';
 import Logo from '../assets/kc designs white logo.svg';
 
 const Navbar = () => {
-  useEffect(() => {
-    // Function to hide preloader on window load
-    const hidePreloader = () => {
-      const preloader = document.getElementById("preloader");
-      if (preloader) {
-        preloader.style.display = "none";
-      }
-    };
-
-    // Function to change navbar color on scroll
-    const changeNavbarColor = () => {
-      const navbar = document.querySelector('.navcas');
-      if (window.scrollY > 50) {
-        navbar.style.backgroundColor = '#0a0a0a';
-      } else {
-        navbar.style.backgroundColor = 'transparent';
-      }
-    };
-
-    // Add event listener for scroll event
-    window.addEventListener('scroll', changeNavbarColor);
-
-    // Clean up event listener on component unmount
-    return () => {
-      window.removeEventListener('scroll', changeNavbarColor);
-    };
-  }, []); // Empty dependency array ensures this effect runs only once on component mount
 
   // Function to toggle navbar color
   const toggleNavbarColor = () => {
